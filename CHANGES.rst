89fb439fe88cb1c0110e9185440f4253380a5949
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-10-26 14:00:37 -0200

    Release 0.1.1

--- CHANGES.rst
+++ CHANGES.rst
@@ -7,6 +7,14 @@ v0.1.0
 * Start project
 * Update README
 
-v0.1.1[unreleased]
+v0.1.1
 -----------
 * Update codeship badge
+* Enable haystack
+* Create project indexes
+* ProjectListView
+
+v1.0.0[unreleased]
+-----------
+* TODO: Implement tests for projectlistview
+* TODO: Implement tests for max queries
--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='ovp-search',
-    version='0.1.0',
+    version='0.1.1',
     author=u'Atados',
     author_email='arroyo@atados.com.br',
     packages=find_packages(),
     url='https://github.com/OpenVolunteeringPlatform/django-ovp-core',
-    download_url = 'https://github.com/OpenVolunteeringPlatform/django-ovp-core/tarball/0.1.0',
+    download_url = 'https://github.com/OpenVolunteeringPlatform/django-ovp-core/tarball/0.1.1',
     license='AGPL',
     description='This module has search functionality for' + \
                 ' ovp projects and nonprofits',
