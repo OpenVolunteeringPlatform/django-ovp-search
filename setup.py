f8fea284541e9c56897d6f3c763f75d32bed49f2
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-11-13 21:36:48 -0200

    Upgrade django-rest-framework to 3.5.3

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     zip_safe=False,
     install_requires = [
       'Django>=1.10.1,<1.11.0',
-      'djangorestframework>=3.4.7,<3.5.0',
+      'djangorestframework>=3.5.3,<3.6.0',
       'codecov>=2.0.5,<2.1.0',
       'coverage>=4.2,<4.3.0',
       'django-haystack>=2.5.0,<2.6.0',
