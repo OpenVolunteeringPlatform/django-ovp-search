b8834f3d0f58a7d0bc0ab1085a22eb10cc637fcb
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-11-27 21:12:13 -0200

    Add whoosh_raw helper

--- ovp_search/helpers.py
+++ ovp_search/helpers.py
@@ -1,7 +1,13 @@
 from haystack import connection_router, connections
+from haystack.inputs import Raw
 
 def is_whoosh_backend():
   backend_alias = connection_router.for_read()
 
   return connections[backend_alias].__class__.__name__ == "WhooshEngine"
 
+
+def whoosh_raw(t):
+  if is_whoosh_backend():
+    return Raw("(\"{}\")".format(t))
+  return t
