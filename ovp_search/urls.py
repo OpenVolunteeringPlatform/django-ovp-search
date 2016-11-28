9bd1c04b6a2bc95efab7b1e61559211cdafb88b9
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-11-28 14:26:52 -0200

    Create OrganizationSearchResource

--- ovp_search/urls.py
+++ ovp_search/urls.py
@@ -3,11 +3,14 @@ from rest_framework import routers
 from ovp_search import views
 
 
-router = routers.DefaultRouter()
-router.register(r'search', views.SearchResource, 'search-list')
+project_search = routers.DefaultRouter()
+project_search.register(r'projects', views.ProjectSearchResource, 'search-projects')
 
+organization_search = routers.DefaultRouter()
+organization_search.register(r'organizations', views.OrganizationSearchResource, 'search-organizations')
 
 urlpatterns = [
-  url(r'^', include(router.urls)),
+  url(r'^search/', include(project_search.urls)),
+  url(r'^search/', include(organization_search.urls)),
   url(r'^search/country-cities/(?P<country>[^/]+)/', views.query_country, name='search-query-country'),
 ]
--- ovp_search/views.py
+++ ovp_search/views.py
@@ -1,5 +1,8 @@
 from ovp_projects.serializers.project import ProjectSearchSerializer
-from ovp_projects import models
+from ovp_projects.models import Project
+from ovp_organizations.models import Organization
+
+from ovp_organizations.serializers import OrganizationSearchSerializer
 
 from ovp_search import helpers
 
@@ -10,10 +13,80 @@ from rest_framework import mixins
 from rest_framework import response
 from rest_framework import decorators
 
-from haystack.query import SearchQuerySet
+from haystack.query import SearchQuerySet, SQ
+
+import json
+
+class OrganizationSearchResource(mixins.ListModelMixin, viewsets.GenericViewSet):
+  serializer_class = OrganizationSearchSerializer
+
+  def get_queryset(self):
+    params = self.request.GET
+
+    key = 'nonprofits-{}'.format(hash(frozenset(params.items())))
+    cache_ttl = 120
+    result = cache.get(key)
+
+    if not result:
+      highlighted = params.get('highlighted') == 'true'
+      query = params.get('query', None)
+      cause = params.get('cause', None)
+      address = params.get('address', None)
+
+      queryset = SearchQuerySet().models(Organization)
+
+      if address:
+        address = json.loads(address)
+        types = []
+        search_region = False
+
+        if u'address_components' in address:
+          for component in address[u'address_components']:
+            for component_type in component[u'types']:
+              if not search_region:
+                type_string = u"{}-{}".format(component[u'long_name'], component_type).strip()
+
+                if component_type == u"colloquial_area":
+                  search_region = type_string
+
+                if type_string not in types:
+                  types.append(type_string)
+
+          # Filter all address components
+          if not search_region:
+            for address_type in types:
+              queryset = queryset.filter(address_components=helpers.whoosh_raw(address_type))
+
+          # User is filtering for Grande São Paulo
+          # We have to hack our way around it
+          else:
+            raise Exception("to be implemented/tested")
+            filters = GoogleRegion.objects.filter(region_name=search_region)
+            keys = [f.filter_by for f in filters]
+            queryset = queryset.filter(address_components__in=keys)
 
 
-class SearchResource(mixins.ListModelMixin, viewsets.GenericViewSet):
+      queryset = queryset.filter(highlighted=True) if highlighted else queryset
+      queryset = queryset.filter(content=query) if query else queryset
+
+      if cause:
+        causes = cause.split(',')
+        q_obj = SQ()
+        for c in causes:
+          q_obj.add(SQ(causes=c), SQ.OR)
+        queryset = queryset.filter(q_obj)
+
+      # haystack SearchQuerySet has to be converted to a django QuerySet
+      # to work properly with django-rest-framework
+      # TODO: Find a solution
+      result_keys = [q.pk for q in queryset]
+      result = Organization.objects.filter(pk__in=result_keys, deleted=False, published=True).prefetch_related('causes').order_by('-highlighted')
+      cache.set(key, result, cache_ttl)
+
+    return result
+
+
+class ProjectSearchResource(mixins.ListModelMixin, viewsets.GenericViewSet):
   serializer_class = ProjectSearchSerializer
 
   def get_queryset(self):
@@ -33,7 +106,7 @@ class SearchResource(mixins.ListModelMixin, viewsets.GenericViewSet):
       address = params.get('address', None)
       highlighted = (params.get('highlighted') == 'true')
 
-      queryset = SearchQuerySet().models(models.Project)
+      queryset = SearchQuerySet().models(Project)
 
       #if nonprofit:
       #  nonprofit = Nonprofit.objects.get(user__slug=nonprofit)
@@ -61,7 +134,7 @@ class SearchResource(mixins.ListModelMixin, viewsets.GenericViewSet):
             # Filter all address components
             if not search_region:
               for address_type in types:
-                queryset = queryset.filter(address_components=address_type)
+                queryset = queryset.filter(address_components=helpers.whoosh_raw(address_type))
 
             # User is filtering for Grande São Paulo
             # We have to hack our way around it
@@ -95,7 +168,7 @@ class SearchResource(mixins.ListModelMixin, viewsets.GenericViewSet):
       # to work properly with django-rest-framework
       # TODO: Find a solution
       result_keys = [q.pk for q in queryset]
-      result = models.Project.objects.filter(pk__in=result_keys, deleted=False, published=True, closed=False).prefetch_related('skills', 'causes', 'work__availabilities').select_related('job', 'work', 'address').order_by('-highlighted')
+      result = Project.objects.filter(pk__in=result_keys, deleted=False, published=True, closed=False).prefetch_related('skills', 'causes').order_by('-highlighted')
       cache.set(key, result, cache_ttl)
 
     return result
@@ -106,7 +179,7 @@ def query_country(request, country):
   available_cities = []
 
   search_term = helpers.whoosh_raw("{}-country".format(country))
-  queryset = SearchQuerySet().models(models.Project).filter(address_components__exact=search_term)
+  queryset = SearchQuerySet().models(Project).filter(address_components__exact=search_term)
 
   for project in queryset:
     for comp in project.address_components:
