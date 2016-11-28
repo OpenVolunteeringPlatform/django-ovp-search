2127e3adf190736e14f8500753ffc58126cb39f4
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-11-28 15:45:26 -0200

    Add test case for index rebuilding

--- ovp_search/tests/test_execution.py
+++ ovp_search/tests/test_execution.py
@@ -1,1 +1,7 @@
 import ovp_search.apps
+from django.test import TestCase
+from django.core.management import call_command
+
+class RebuildIndexTestCase(TestCase):
+  def test_rebuild_index_execution(self):
+    call_command('rebuild_index', '--noinput', verbosity=0)
