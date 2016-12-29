66d8e2dfd3c434eeaf29c23670dfef279f3333fd
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-12-29 11:58:28 -0200

    Release 1.0.5

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='ovp-search',
-    version='1.0.4',
+    version='1.0.5',
     author=u'Atados',
     author_email='arroyo@atados.com.br',
     packages=find_packages(),
     url='https://github.com/OpenVolunteeringPlatform/django-ovp-core',
-    download_url = 'https://github.com/OpenVolunteeringPlatform/django-ovp-core/tarball/1.0.4',
+    download_url = 'https://github.com/OpenVolunteeringPlatform/django-ovp-core/tarball/1.0.5',
     license='AGPL',
     description='This module has search functionality for' + \
                 ' ovp projects and nonprofits',
