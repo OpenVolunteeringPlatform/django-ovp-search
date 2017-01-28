a76de5c396c44399ab5825c48fa139e1d6a7a470
Jonathan Barratt <jonathan.barratt@gmail.com>
2017-01-28 09:30:07 +0700

    update project name used in url args for setup
    s/django-ovp-core/django-ovp-search/

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     author=u'Atados',
     author_email='arroyo@atados.com.br',
     packages=find_packages(),
-    url='https://github.com/OpenVolunteeringPlatform/django-ovp-core',
-    download_url = 'https://github.com/OpenVolunteeringPlatform/django-ovp-core/tarball/1.0.8',
+    url='https://github.com/OpenVolunteeringPlatform/django-ovp-search',
+    download_url = 'https://github.com/OpenVolunteeringPlatform/django-ovp-search/tarball/1.0.8',
     license='AGPL',
     description='This module has search functionality for' + \
                 ' ovp projects and nonprofits',
