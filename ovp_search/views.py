99c1c3b898aae637605511927f91de20aa0ce836
Leonardo Arroyo <contato@leonardoarroyo.com>
2017-04-07 12:42:05 -0300

    Fix broken import

--- ovp_search/views.py
+++ ovp_search/views.py
@@ -19,7 +19,7 @@ from rest_framework import viewsets
 from rest_framework import mixins
 from rest_framework import response
 from rest_framework import decorators
-from rest_framework import filters
+from rest_framework.filters import OrderingFilter
 
 from haystack.query import SearchQuerySet, SQ
 
